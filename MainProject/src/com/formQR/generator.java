7a29733cd656ad6fc7341cda111f4837033fb614
Stevens Cambranes <cambranesstevens@gmail.com>
2025-10-15 14:31:10 -0600

    Iniciando proyecto en JAVA

---
+++ MainProject/src/com/formQR/generator.java
@@ -0,0 +1,11 @@
+package com.formQR;
+
+import javax.swing.*;
+
+public class generator {
+    private JPanel mainPanel;
+    private JTextField txtURL;
+    private JLabel lblTittle;
+    private JLabel lblInsertURL;
+    private JButton btnGenerator;
+}
